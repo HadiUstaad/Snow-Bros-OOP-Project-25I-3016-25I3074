0b3137dcec945131fb359e2fbe1ab22cbf1122b1
HadiUstaad <i253016@isb.nu.edu.pk>
2026-04-20 06:24:56 +0500

    Created Gameobject.h
    an abstract class that further will contain enemies bosses etc

---
+++ VS_Project_Files/VS_Project_Files/GameObjects.h
@@ -0,0 +1,30 @@
+#pragma once
+
+using namespace std;
+
+class GameObject
+{
+	float posX;
+	float posY;
+	bool isActive; // is it in the game
+	float width;	// set width and height of collision of gameobject
+	float height;
+
+public:
+	GameObject(float x, float y, bool active, float w, float h);
+	virtual ~GameObject();
+
+	float getX();
+	float getY();
+	float getWidth();
+	float getHeight();
+	bool getActive();
+
+	void setPositiion(float x, float y);
+	void setColliders(float x, float y);
+	void setActive(bool active);
+
+	virtual void draw() = 0; // draws the object on the screen. it is differnt for each object
+	// therefore is abstract
+
+};
